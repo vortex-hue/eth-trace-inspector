e871ebba3d052e75e5fb7d907462cc708214ba26
Peter Benjamin Ani <benjaminparish6@gmail.com>
2024-06-22 13:52:38 +0100

    fix: handle bug

--- src/rpc.ts
+++ src/rpc.ts
@@ -21,6 +21,7 @@ export async function getProvider(
  if (rpcUrl) {
  const rpcProvider = new JsonRpcProvider(rpcUrl);
  // TODO
+  // Note
  const network = await rpcProvider.getNetwork();
  return { provider: rpcProvider, chainId: Number(network.chainId) };
  }
