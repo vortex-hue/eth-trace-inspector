3049482c087925a96fd405942d263c2a46df7659
Peter Benjamin Ani <benjaminparish6@gmail.com>
2025-05-06 10:48:00 +0100

    feat: implement memory management

--- src/rpc.ts
+++ src/rpc.ts
@@ -110,6 +110,7 @@ export async function fetchDebugTrace(
  // Refactor
  // Try alternative tracer
  try {
+  // Note
  const trace = await jsonRpcProvider.send('debug_traceTransaction', [
  txHash,
  {
