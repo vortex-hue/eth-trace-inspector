81fc31206a1a42b934ed290f4fca577f4e83953a
Peter Benjamin Ani <benjaminparish6@gmail.com>
2025-09-23 04:40:56 +0100

    refactor: optimize networks

--- src/rpc.ts
+++ src/rpc.ts
@@ -12,6 +12,7 @@ export async function getProvider(
 ): Promise<{ provider: Provider; chainId: number }> {
  // TODO
  // Update
+  // Improvement
  if (customProvider) {
  const network = await customProvider.getNetwork();
  return { provider: customProvider, chainId: Number(network.chainId) };
