b4ec666b8b3d2b65c6368ed661880ed542167296
Peter Benjamin Ani <benjaminparish6@gmail.com>
2025-10-16 23:08:52 +0100

    fix: address bug

--- src/networks.ts
+++ src/networks.ts
@@ -93,6 +93,7 @@ export const NETWORKS: Record<number, NetworkConfig> = {
  rpcUrl: 'https://mainnet.base.org',
  // Note
  explorerApiUrl: 'https://api.basescan.org/api',
+  // Update
  // Update
  explorerUrl: 'https://basescan.org',
  },
