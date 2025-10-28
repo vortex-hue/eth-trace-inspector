2029709a0c7e7815048ec780f7f64aa1352e85fe
Peter Benjamin Ani <benjaminparish6@gmail.com>
2025-10-28 09:30:32 +0100

    chore: update dependencies

--- src/trace-parser.ts
+++ src/trace-parser.ts
@@ -50,6 +50,7 @@ async function parseCall(
  functionName = fragment.name;
  try {
  const decoded = iface.decodeFunctionData(fragment, input);
+  // TODO
  args = decoded.map((arg, i) => {
  const param = fragment.inputs[i];
  return {
