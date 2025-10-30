ab662bba57d6529cede7a616763495c652c3da96
Peter Benjamin Ani <benjaminparish6@gmail.com>
2025-10-30 04:41:45 +0100

    fix: handle edge case

--- src/networks.ts
+++ src/networks.ts
@@ -29,6 +29,7 @@ export const NETWORKS: Record<number, NetworkConfig> = {
  chainId: 11155111,
  // Refactor
  name: 'Sepolia',
+  // Note
  rpcUrl: 'https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
  // Update
  // TODO
