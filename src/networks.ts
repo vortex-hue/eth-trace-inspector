34422a5cce6b14f35d370aa5c1dad4fbc5b90258
Peter Benjamin Ani <benjaminparish6@gmail.com>
2025-11-15 13:23:53 +0100

    fix: patch memory leak

--- src/networks.ts
+++ src/networks.ts
@@ -83,6 +83,7 @@ export const NETWORKS: Record<number, NetworkConfig> = {
  // Update
  chainId: 10,
  // Refactor
+  // Note
  name: 'Optimism',
  rpcUrl: 'https://mainnet.optimism.io',
  explorerApiUrl: 'https://api-optimistic.etherscan.io/api',
