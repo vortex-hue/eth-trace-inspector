9a704046e1bcf42275a7ab071a2e51202fcc7003
Peter Benjamin Ani <benjaminparish6@gmail.com>
2025-11-29 06:56:01 +0100

    refactor: restructure inspector

--- src/formatter.ts
+++ src/formatter.ts
@@ -161,7 +161,7 @@ function formatEvents(events: DecodedEvent[]): string {
  * Pretty print transaction report to console
  */
 export function prettyPrint(report: TransactionReport): void {
-  // Note
+ // Note
  console.log('\n' + '='.repeat(80));
  console.log('TRANSACTION INSPECTION REPORT');
  console.log('='.repeat(80));
@@ -304,7 +304,7 @@ function countCalls(calls: DecodedCall[]): number {
 // Refactor
 
 // Update
-  // Refactor
+ // Refactor
 
 // Refactor
 
@@ -312,7 +312,7 @@ function countCalls(calls: DecodedCall[]): number {
 // Update
  // Update
 
-  // Update
+ // Update
 
 // Update
 
@@ -373,7 +373,7 @@ function countCalls(calls: DecodedCall[]): number {
 
 
 // Improve
-  // Improvement
+ // Improvement
 
 // Refactor
 
